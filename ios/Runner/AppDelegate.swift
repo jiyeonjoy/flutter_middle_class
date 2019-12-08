4fce6ba5851de9aecbc04fd17238a8b43aeadd67
jiyeonjoy <choi02647@naver.com>
2019-12-08 20:40:01 +0900

    네이티브 연동 iOS 값 전달 성공!!

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,18 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    let controller = self.window.rootViewController as! FlutterViewController
+
+    let channel = FlutterMethodChannel.init(name: "example.com/value", binaryMessenger: controller)
+    channel.setMethodCallHandler { (call, result) in
+        if (FlutterMethodCall.method == "getValue") {
+            result("성공")
+        } else {
+            result (FlutterMethodNotImplemented)
+        }
+    }
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
