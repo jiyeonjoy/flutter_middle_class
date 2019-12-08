2b7b113ca47c078d046a7dc08ba7a754f561d6b3
jiyeonjoy <choi02647@naver.com>
2019-12-08 17:37:02 +0900

    AnimatedContainer 완성!

--- lib/my_animated_container.dart
+++ lib/my_animated_container.dart
@@ -8,8 +8,12 @@ class MyAnimatedContainer extends StatefulWidget {
 }
 
 class _MyAnimatedContainerState extends State<MyAnimatedContainer> {
+
   double _width = 50;
   double _height = 50;
+  Color _color = Colors.green;
+  BorderRadius _borderRadius = BorderRadius.circular(8);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,8 +24,16 @@ class _MyAnimatedContainerState extends State<MyAnimatedContainer> {
         child: AnimatedContainer(
             width: _width,
             height: _height,
-            color: Colors.green,
-            duration: Duration(seconds: 1)),
+            // color 무조건 BoxDecoration 안에 넣어줘야됨!!
+            decoration: BoxDecoration(
+                color: _color,
+                // 가장자리 곡선 정의
+                borderRadius: _borderRadius
+            ),
+            duration: Duration(seconds: 1),
+            // 스피드감 추가 다양한기능 도전해 보길!!
+            curve: Curves.fastOutSlowIn,
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -29,6 +41,9 @@ class _MyAnimatedContainerState extends State<MyAnimatedContainer> {
             final random = Random();
             _width = random.nextInt(300).toDouble();
             _height = random.nextInt(300).toDouble();
+            _color = Color.fromRGBO(
+                random.nextInt(256), random.nextInt(256), random.nextInt(256), 1);
+            _borderRadius = BorderRadius.circular(random.nextInt(100).toDouble());
           });
         },
         child: Icon(Icons.play_arrow),
